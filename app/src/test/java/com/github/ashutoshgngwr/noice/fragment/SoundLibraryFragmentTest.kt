aab11a24ed9279212617163fb1c85ce261c2963c
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2019-06-07 01:36:39 +0530

    test: add few cases for SoundLibraryFragment

--- app/src/test/java/com/github/ashutoshgngwr/noice/fragment/SoundLibraryFragmentTest.kt
+++ app/src/test/java/com/github/ashutoshgngwr/noice/fragment/SoundLibraryFragmentTest.kt
@@ -7,6 +7,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows.shadowOf
 
 @RunWith(RobolectricTestRunner::class)
 class SoundLibraryFragmentTest {
@@ -29,5 +30,25 @@ class SoundLibraryFragmentTest {
       }
   }
 
+  @Test
+  fun `should refresh recycler view on playback state change`() {
+    fragmentScenario
+      .moveToState(Lifecycle.State.CREATED)
+      .onFragment { fragment ->
+        shadowOf(fragment.mRecyclerView).setDidRequestLayout(false)
+        fragment.onPlaybackStateChanged()
+        assert(shadowOf(fragment.mRecyclerView).didRequestLayout())
+      }
+  }
+
+  @Test
+  fun `should destroy nice and quiet`() {
+    fragmentScenario
+      .moveToState(Lifecycle.State.DESTROYED)
+  }
+
   // not sure how to write more cases..?
+  // UPDATE:
+  // okay! this is the second time I am having trouble writing unit tests with Robolectric.
+  // Maybe UI tests are better off with an instrumented framework like Espresso..?
 }
