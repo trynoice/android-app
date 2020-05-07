89aa3d1a0fe6fae80b0589830b6b7c79cc5a6678
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2020-05-07 18:06:34 +0530

    fix(CastOptionsProvider): disable default media session when casting
    we don't use media session in any way. Moreover, using a media session
    forces us to provide media metadata that we don't have. Even if we put
    filler metadata to just to make everything look consistent,
    'CastSession.remoteMediaClient' won't accept it without a valid media
    URL which we don't have.

--- app/src/main/java/com/github/ashutoshgngwr/noice/cast/CastOptionsProvider.kt
+++ app/src/main/java/com/github/ashutoshgngwr/noice/cast/CastOptionsProvider.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.github.ashutoshgngwr.noice.R
 import com.google.android.gms.cast.framework.CastOptions
 import com.google.android.gms.cast.framework.OptionsProvider
+import com.google.android.gms.cast.framework.media.CastMediaOptions
 
 @Suppress("unused")
 class CastOptionsProvider : OptionsProvider {
@@ -13,6 +14,12 @@ class CastOptionsProvider : OptionsProvider {
       // `cast_app_id` is declared in 'app/build.gradle'
       setReceiverApplicationId(context.getString(R.string.cast_app_id))
       setStopReceiverApplicationWhenEndingSession(true)
+      setCastMediaOptions(
+        CastMediaOptions.Builder()
+          .setMediaSessionEnabled(false)
+          .build()
+      )
+
       build()
     }
   }
