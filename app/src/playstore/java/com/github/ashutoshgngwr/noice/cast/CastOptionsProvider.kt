176c32d1b10da45a223683cb8420fb8974fa6ff5
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2020-09-01 19:06:03 +0530

    chore(app): fix lint warnings

--- app/src/playstore/java/com/github/ashutoshgngwr/noice/cast/CastOptionsProvider.kt
+++ app/src/playstore/java/com/github/ashutoshgngwr/noice/cast/CastOptionsProvider.kt
@@ -5,6 +5,7 @@ import com.github.ashutoshgngwr.noice.BuildConfig
 import com.github.ashutoshgngwr.noice.R
 import com.google.android.gms.cast.framework.CastOptions
 import com.google.android.gms.cast.framework.OptionsProvider
+import com.google.android.gms.cast.framework.SessionProvider
 import com.google.android.gms.cast.framework.media.CastMediaOptions
 
 @Suppress("unused")
@@ -34,5 +35,5 @@ class CastOptionsProvider : OptionsProvider {
     }
   }
 
-  override fun getAdditionalSessionProviders(context: Context?) = null
+  override fun getAdditionalSessionProviders(context: Context?): List<SessionProvider>? = null
 }
