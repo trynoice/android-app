0f34737d48d307122f67bd17fcf2527661c6669e
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2022-04-29 13:21:52 +0530

    feat(app): add Flow APIs to collect latest values from the Settings repo

--- app/src/main/java/com/github/ashutoshgngwr/noice/ext/SharedPreferencesExt.kt
+++ app/src/main/java/com/github/ashutoshgngwr/noice/ext/SharedPreferencesExt.kt
@@ -1,6 +1,9 @@
 package com.github.ashutoshgngwr.noice.ext
 
 import android.content.SharedPreferences
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 
 /**
  * [getMutableStringSet] returns a mutable copy of the original [Set]<[String]> returned by
@@ -11,4 +14,16 @@ import android.content.SharedPreferences
  */
 fun SharedPreferences.getMutableStringSet(key: String): MutableSet<String> {
   return getStringSet(key, null)?.toMutableSet() ?: mutableSetOf()
+}
+
+/**
+ * Returns a [callback Flow][callbackFlow] that emits a key from [this] [SharedPreferences] every
+ * time its value changes.
+ */
+fun SharedPreferences.keysFlow(): Flow<String> = callbackFlow {
+  val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, key: String? ->
+    key?.also { trySend(it) }
+  }
+  registerOnSharedPreferenceChangeListener(listener)
+  awaitClose { unregisterOnSharedPreferenceChangeListener(listener) }
 }
