28b3ee1719a344704a7005a2ee5aa2896ba7b474
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2022-10-19 00:30:51 +0530

    feat(app): remove may gradle dependency

--- app/src/main/java/com/github/ashutoshgngwr/noice/NoiceApplication.kt
+++ app/src/main/java/com/github/ashutoshgngwr/noice/NoiceApplication.kt
@@ -27,7 +27,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import io.github.ashutoshgngwr.may.May
 import java.io.File
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -80,20 +79,13 @@ class NoiceApplication : Application(), Configuration.Provider {
 
   @Module
   @InstallIn(SingletonComponent::class)
-  object CacheStoreModule {
-    @Provides
-    @Singleton
-    fun cacheStore(@ApplicationContext context: Context): May {
-      return May.openOrCreateDatastore(context, "app-cache.may.db", Context.MODE_PRIVATE)
-    }
-  }
-
-  @Module
-  @InstallIn(SingletonComponent::class)
   object AppDatabaseModule {
     @Provides
     @Singleton
     fun appDatabase(@ApplicationContext context: Context): AppDatabase {
+      // TODO: remove in future versions
+      // clear old cache data store.
+      context.deleteDatabase("app-cache.may.db")
       return Room.databaseBuilder(context, AppDatabase::class.java, "${context.packageName}.db")
         .build()
     }
