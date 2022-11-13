aee28d4ed33ee02f8c86aae02d09a1474f9c3e26
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2022-11-13 17:16:01 +0530

    fix(app): make all sound metadata models serializable

--- app/src/main/java/com/github/ashutoshgngwr/noice/models/SoundTag.kt
+++ app/src/main/java/com/github/ashutoshgngwr/noice/models/SoundTag.kt
@@ -1,9 +1,10 @@
 package com.github.ashutoshgngwr.noice.models
 
 import com.github.ashutoshgngwr.noice.data.models.SoundTagDto
+import java.io.Serializable
 import com.trynoice.api.client.models.SoundTag as ApiSoundTag
 
-data class SoundTag(val id: String, val name: String)
+data class SoundTag(val id: String, val name: String) : Serializable
 
 fun ApiSoundTag.toRoomDto(): SoundTagDto {
   return SoundTagDto(id = id, name = name)
